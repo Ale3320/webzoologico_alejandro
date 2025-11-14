e7528f8f78cf77ccab65421a7e79d52a13e53813
Ale3320 <alejandro.parada2307@gmail.com>
2025-11-13 20:13:47 -0500

    Se agrega PUT

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -4,45 +4,82 @@ import { AnimalService } from '../../services/animal-service';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-animal-component',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
 })
 export class AnimalComponent {
-newAnimalEntry() {
-throw new Error('Method not implemented.');
-}
   animalList: any = [];
   animalForm: FormGroup | any;
+  idAnimal: any;
+  editableAnimal: boolean = false;
 
-  constructor(private animalService: AnimalService,
+  constructor(
+    private animalService: AnimalService,
     private toastr: ToastrService,
     private router: Router,
-    private formBuilder: FormBuilder) {}
-  
+    private formBuilder: FormBuilder
+  ) {}
+
   getAllAnimals() {
     //subscribe crea una varibale data: la lista de animales
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
       this.animalList = data;
     });
   }
-  
-  newMessage(messageText: string) {
-    this.toastr.success('Clic aquí para actualizar la lista', messageText)
-      .onTap
-      .pipe(take(1))
-      .subscribe(() => window.location.reload());//hacer refresco a la pagina
+
+  newAnimalEntry() {
+    this.animalService.newAnimal(this.animalForm.value).subscribe(() => {
+      //Redirigiendo a la ruta actual /inicio y recargando la ventana
+      this.router.navigate(['/animales']).then(() => {
+        this.newMessage('Registro exitoso');
+      });
+    });
   }
-  
+
+  updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(() => {
+      //Enviando mensaje de confirmación
+      this.newMessage('Animal editado');
+    });
+  }
+
+  toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal);
+    this.animalService.getOneAnimal(id).subscribe((data) => {
+      this.animalForm.setValue({
+        nombre: data.nombre,
+        edad: data.edad,
+        tipo: data.tipo,
+      });
+    });
+    this.editableAnimal = !this.editableAnimal;
+  }
+
+  newMessage(messageText: string) {
+    this.toastr
+      .success('Clic aquí para actualizar la lista', messageText)
+      .onTap.pipe(take(1))
+      .subscribe(() => window.location.reload()); //hacer refresco a la pagina
+  }
+
   ngOnInit() {
     this.getAllAnimals();
     this.animalForm = this.formBuilder.group({
       nombre: '',
       edad: 0,
-      tipo: ''
-    })
+      tipo: '',
+    });
   }
 }
--- src/app/services/animal-service.ts
+++ src/app/services/animal-service.ts
@@ -15,11 +15,21 @@ export class AnimalService {
     //get es el verbo HTTP GET
     return this.http.get<any>(this.apiUri);
   }
-  
+
   newAnimal(data: any): Observable<any> {
-    return this.http.post<any>(
-      this.apiUri,
-      data,
-      { headers: this.httpOptions });
+    return this.http.post<any>(this.apiUri, data, { headers: this.httpOptions });
+  }
+
+  updateAnimal(id: any, data: any): Observable<any> {
+    console.log(data);
+    return this.http.put<any>(this.apiUri + '/' + id, data, { headers: this.httpOptions });
+  }
+
+  getOneAnimal(id: any): Observable<any> {
+    return this.http.get<any>(this.apiUri + '/' + id, { headers: this.httpOptions });
+  }
+
+  deleteAnimal(id: any) {
+    return this.http.delete<any>(this.apiUri + '/' + id, { headers: this.httpOptions });
   }
 }
